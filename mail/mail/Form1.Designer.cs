d61af8ec5cd70bd72af9da2145dbede47f67ad04
njekk <jekkonenniko@gmail.com>
2022-04-17 10:41:37 +0300

    Topille postia

---
+++ mail/mail/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace mail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(583, 226);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.AcceptsReturn = true;
+            this.tbMessage.Location = new System.Drawing.Point(55, 43);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(254, 68);
+            this.tbMessage.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbMessage;
+    }
+}
+
---
+++ mail/mail/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Web;
+using System.Net;
+
+namespace mail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string from = "NootWare@gmail.com";
+            string to = "topi.leinonen@edu.savonia.fi";
+            string subject = "Tärkeää postia, olkaa hyvä";
+            try
+            {
+                string Text = tbMessage.Text;
+                string pass = "Noottiware"; //onetime password from google
+                SmtpClient mailClient = new SmtpClient("smtp.gmail.com");
+                mailClient.EnableSsl = true;
+                mailClient.Port = 587;
+                mailClient.Credentials = new System.Net.NetworkCredential(from, pass);
+                MailMessage msgMail = new MailMessage(from, to, subject, Text);
+                mailClient.Send(msgMail);
+                MessageBox.Show("Your Mail is sended");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
